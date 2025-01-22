191b3ab3f6808c14a0ed964927deb89015ef2f3e
Gaurav <vapourise00@gmail.com>
2025-01-22 19:26:45 +0530

    finished the edit task feature

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,19 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Task({ task, deleteTask, editTask }) {
+  const [editedTaskValue, setEditedTaskValue] = useState(task.taskValue)
   const [editState, setEditState] = useState(false)
+  const handleOnChange = (e) => {
+    setEditedTaskValue(e.target.value)
+  }
+
   const handleOnDelete = () => {
     deleteTask(task)
   }
   const handleOnEdit = () => {
-    editTask()
+    const editedTask = { id: task.id, taskValue: editedTaskValue }
+    editTask(editedTask)
   }
   return (
     <div className="container d-flex">
-      {editState == false ? (
+      {!editState ? (
         <button
           type="button"
-          class="btn btn-success"
+          className="btn btn-success"
           onClick={() => {
             setEditState(true)
           }}
@@ -23,29 +29,30 @@ function Task({ task, deleteTask, editTask }) {
       ) : (
         <button
           type="button"
-          class="btn btn-success"
+          className="btn btn-success"
           onClick={() => {
             setEditState(false)
+            handleOnEdit()
           }}
         >
           Ok
         </button>
       )}
-      {editState == false ? (
-        <div class="container-md border border-success rounded">
+      {!editState ? (
+        <div className="container-md border border-success rounded">
           {task.taskValue}
         </div>
       ) : (
         <input
           type="text"
-          class="form-control"
+          className="form-control"
           aria-label="Recipient's username"
           aria-describedby="button-addon2"
-          value={task.taskValue}
+          value={editedTaskValue}
           onChange={handleOnChange}
         />
       )}
-      <button type="button" class="btn btn-danger" onClick={handleOnDelete}>
+      <button type="button" className="btn btn-danger" onClick={handleOnDelete}>
         Delete
       </button>
     </div>
